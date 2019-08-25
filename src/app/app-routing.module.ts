a550d132738287b8584c7b8aaa18e2883ab91192
codeconcept <contact@codeconcept.fr>
2019-08-25 19:09:53 +0200

    articles module lazy loaded + boostrap with sticky footer and menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
