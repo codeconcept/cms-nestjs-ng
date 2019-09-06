25bcc87a379a6f156112ecbcc820fac67830b688
codeconcept <contact@codeconcept.fr>
2019-09-06 14:35:50 +0200

    posting a new user for registration

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseURL = 'http://localhost:3000/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(credentials) {
+    const fullURL = `${this.baseURL}/register`;
+    this.http
+      .post<any>(fullURL, credentials)
+      .subscribe(createdUser => {
+        console.log('createdUser', createdUser);
+      });
+  }
+
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -29,6 +29,7 @@ export class RegisterComponent implements OnInit {
 
   submit() {
     console.log(this.registerForm.value);
+    this.authService.register(this.registerForm.value);
   }
 
 }
